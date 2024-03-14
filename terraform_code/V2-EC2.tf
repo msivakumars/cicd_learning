6d80de3c2959c1ce54613eb8919e9dab06ca3a88
msivakumars@hotmail.com <msivakumars@hotmail.com>
2024-03-14 21:24:06 +0530

    Modification done to v2-ec2.tf

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "demo-server" {
   ami = "ami-0f403e3180720dd7e"
   instance_type = "t2.micro"
   key_name = "dpp"
-  security_groups = [ aws_security_group.demo-sg.id ]
+  security_groups = [ "Sample_SG"]
 }
 
 resource "aws_security_group" "demo-sg" {
